5eebe96cbc1b46b69a19067e7d10d9491e17d113
sphrne <sphrne@yahoo.com>
2023-12-13 13:29:49 +0100

    implemented main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public Tippreihen getInput(String nameString) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String lottoName = scanner.next();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return null;
+
+    } 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hallo Herr Keller, willkommen bei der Lotterie App!");
+        System.out.print("Möchten Sie heute \"Lotto\" oder \"Eurojackpot\" spielen?");
+        // add getInput Class here!!!
+
     }
 }
